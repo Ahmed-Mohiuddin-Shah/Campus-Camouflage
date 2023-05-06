6ac7fb7885b6f10c66c75c660c570cdb6fc2e743
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-05-06 19:05:26 +0500

    Lots of stuff

---
+++ Game.java
@@ -0,0 +1,92 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
+
+public class Game implements ActionListener {
+    private JFrame frame;
+
+    private JButton clientButton, serverButton, settingsButton, exitButton;
+
+    Game() {
+        Font helloHeadline = new Font("", Font.PLAIN, 0);
+        try {
+            helloHeadline = Font.createFont(Font.TRUETYPE_FONT, new File("resources/HelloHeadline.ttf"))
+                    .deriveFont(36f);
+        } catch (Exception e) {
+        }
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(helloHeadline);
+
+        frame = new JFrame("Hide and SEECS");
+        JPanel buttonPanel = new JPanel(new GridLayout(4, 1));
+
+        clientButton = new JButton("Connect to Server");
+        serverButton = new JButton("Create Server");
+        settingsButton = new JButton("Settings");
+        exitButton = new JButton("Exit");
+
+        clientButton.addActionListener(this);
+        serverButton.addActionListener(this);
+        settingsButton.addActionListener(this);
+        exitButton.addActionListener(this);
+
+        buttonPanel.add(clientButton);
+        buttonPanel.add(serverButton);
+        buttonPanel.add(settingsButton);
+        buttonPanel.add(exitButton);
+
+        buttonPanel.setFont(helloHeadline);
+        buttonPanel.setFont(helloHeadline);
+        buttonPanel.setFont(helloHeadline);
+        buttonPanel.setFont(helloHeadline);
+
+        JLabel l = new JLabel("Hide and SEECS", JLabel.CENTER);
+        l.setFont(helloHeadline);
+        l.setForeground(Color.BLACK);
+        frame.add(l, BorderLayout.CENTER);
+        frame.setLayout(new BorderLayout(10, 10));
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1280, 720);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
+        new Game();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Connect to Server":
+                new ClientConnectScreen();
+                frame.dispose();
+                break;
+            case "Create Server":
+                new ServerCreationGUI();
+                frame.dispose();
+                break;
+            case "Settings":
+                new Settings();
+                // frame.dispose();
+                break;
+            case "Exit":
+                frame.dispose();
+                break;
+            default:
+        }
+
+    }
+    // clientButton = new JButton("Connect to Server");
+    // serverButton = new JButton("Create Server");
+    // settingsButton = new JButton("Settings");
+    // exitButton = new JButton("Exit");
+}
---
+++ ServerCreationGUI.java
@@ -0,0 +1,85 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
+
+public class ServerCreationGUI implements ActionListener {
+    private JFrame frame;
+
+    private JButton createButton, cancelButton;
+
+    ServerCreationGUI() {
+        Font helloHeadline = new Font("", Font.PLAIN, 0);
+        try {
+            helloHeadline = Font.createFont(Font.TRUETYPE_FONT, new File("resources/HelloHeadline.ttf"))
+                    .deriveFont(36f);
+        } catch (Exception e) {
+        }
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(helloHeadline);
+
+        frame = new JFrame("Create Server");
+
+        createButton = new JButton("Create");
+        cancelButton = new JButton("Cancel");
+
+        createButton.addActionListener(this);
+        cancelButton.addActionListener(this);
+
+        JLabel l = new JLabel("IP Address", JLabel.CENTER);
+        JLabel l2 = new JLabel("Port", JLabel.CENTER);
+
+        JTextField ip = new JTextField();
+        JTextField port = new JTextField();
+        ip.setFont(helloHeadline);
+        ip.setForeground(Color.BLACK);
+        port.setFont(helloHeadline);
+        port.setForeground(Color.BLACK);
+        createButton.setFont(helloHeadline);
+        createButton.setForeground(Color.BLACK);
+        cancelButton.setFont(helloHeadline);
+        cancelButton.setForeground(Color.BLACK);
+
+        l.setFont(helloHeadline);
+        l.setForeground(Color.BLACK);
+        l2.setFont(helloHeadline);
+        l2.setForeground(Color.BLACK);
+
+        frame.setLayout(new GridLayout(3, 2));
+
+        frame.add(l);
+        frame.add(ip);
+        frame.add(l2);
+        frame.add(port);
+        frame.add(cancelButton);
+        frame.add(createButton);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
+        new Game();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Create":
+                frame.dispose();
+                break;
+            case "Cancel":
+                new Game();
+                frame.dispose();
+                break;
+            default:
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    }
+}
