0ce0a4f320d5fb6a28338ddd2524a7c9129f27c5
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-05-07 11:04:25 +0500

    Removed unused local variable

--- ServerCreationGUI.java
+++ ServerCreationGUI.java
@@ -69,13 +69,12 @@ public class ServerCreationGUI implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case "Create":
-                Server s;
                 if (ip.getText().equals("") && port.getText().equals("")) {
-                    s = new Server();
+                    new Server();
                 } else if (ip.getText().equals("") && !port.getText().equals("")) {
-                    s = new Server(port.getText());
+                    new Server(port.getText());
                 } else {
-                    s = new Server(ip.getText(), port.getText());
+                    new Server(ip.getText(), port.getText());
                 }
                 frame.dispose();
                 break;
